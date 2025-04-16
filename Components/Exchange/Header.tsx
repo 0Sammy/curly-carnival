ee10dc732be48038ea3db4cb3e9f9295631a1c66
Priest <Charleschukwuemeka47@gmail.com>
2025-04-16 14:46:24 +0100

    Feat: Make client edits

--- Components/Exchange/Header.tsx
+++ Components/Exchange/Header.tsx
@@ -10,8 +10,8 @@ const Header = () => {
     return (
         <main className="top-0 left-0 z-[3] fixed flex justify-between items-center bg-white px-4 sm:px-6 py-4 border-primaryGreen border-b w-full text-deepBlack">
             <div className="flex items-center gap-x-2">
-                <Image src={logo} alt="logo" className="w-10" />
-                <h1 className="font-medium text-lg md:text-xl xl:text-2xl">MINEREUM BSC</h1>
+                <Image src={logo} alt="logo" className="w-8" />
+                <h1 className="font-medium text-base">MINEREUM BSC</h1>
             </div>
             <ConnectWallet />
         </main>
